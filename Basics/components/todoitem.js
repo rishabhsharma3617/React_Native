432982c709723854b7da63ca644d713546dbad16
Rishabh Sharma <rishabhsharma3617@gmail.com>
2019-12-31 18:21:39 +0530

    Understood about icons and their use

--- Basics/components/todoitem.js
+++ Basics/components/todoitem.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput, Image , ScrollView , TouchableOpacity, FlatList} from 'react-native';
-
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 export default function TodoItem(props) {
     return(
-        <TouchableOpacity onPress={() => props.deleteItem(props.item.key)}>
-            <Text style={styles.item}>{props.item.text}</Text>
+        <TouchableOpacity >
+            <View style={styles.item}>
+            <MaterialCommunityIcons name='delete' size={18} color={'#333'} onPress={() => props.deleteItem(props.item.key)}/>
+            <Text style={styles.textItem}>{props.item.text}</Text>
+            </View>
         </TouchableOpacity>
     )
 }
@@ -15,6 +18,10 @@ const styles = StyleSheet.create({
         marginTop : 16,
         borderColor : '#bbb',
         borderWidth : 2,
-        borderRadius : 10
+        borderRadius : 10,
+        flexDirection : 'row'
+    },
+    textItem : {
+        marginLeft : 10
     }
 })
